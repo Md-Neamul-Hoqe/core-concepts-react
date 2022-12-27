b8264e8323911dc8d81df67206598b95a7f502d6
Md-Neamul-Hoqe <hoqe1997@gmail.com>
2022-12-27 08:51:47 +0600

    similar in pattern

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function coreConcepts() {
+  /* Define what will be shown in index.html */
   return (
-    <div className="App">
+    /* return only one element so all must in a element like div/section/nav etc. */
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>See You Tomorrow</p>
+        <Person name="Md. Neamul Hoqe"></Person>
       </header>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
 
-export default App;
+/* Define a similar in pattern object */
+function Person(whats) {
+  console.log(whats);
+  const div = {
+    textAlign: "center",
+    width: "800px",
+    border: "1px solid yellow",
+    padding: "10px 20px",
+  };
+  const h1 = {
+    border: "10px groove yellow",
+    padding: "0px 10px",
+    margin: "0",
+  };
+  const h3 = {
+    border: "1px dashed red",
+    margin: "1px auto",
+    // textAlign: "center",
+  };
+
+  /* back data to Person tag where invoked */
+  return (
+    <section>
+      <div style={div}>
+        <h1 style={h1}>{whats.name}</h1>
+        <h3 style={h3}>This is subtitle of this page</h3>
+      </div>
+    </section>
+  );
+}
+
+/* Call for export to index.html */
+export default coreConcepts;
